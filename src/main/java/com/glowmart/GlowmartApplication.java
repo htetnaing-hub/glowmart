8eb68fa211dc1989ddb8a075a755ac7a75c1ca1b
Htet Naing Aung <htetnaing.ucsmdy@gmail.com>
2025-01-06 23:32:55 +0630

    Initial Commit

---
+++ src/main/java/com/glowmart/GlowmartApplication.java
@@ -0,0 +1,13 @@
+package com.glowmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlowmartApplication.class, args);
+	}
+
+}
